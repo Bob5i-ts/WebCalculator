8f0a581bfc1bf4244a959656a01e8a2ec2058be9
Bob5i-ts <91635735+Bob5i-ts@users.noreply.github.com>
2022-07-20 23:18:33 +0300

    Select main elements

---
+++ calc.js
@@ -0,0 +1,6 @@
+const n1 = (...x) => $('#num1').val(...x);
+const n2 = (...x) => $('#num2').val(...x);
+const sign = (...x) => $('#sign').val(...x);
+const equal = (...x) => $('#equal').val(...x);
+const result = (...x) => $('#result').val(...x);
+
