fa604b06a294b449a82eaaa64e99e023881dc2ba
Bob5i-ts <91635735+Bob5i-ts@users.noreply.github.com>
2022-07-20 23:39:28 +0300

    Add number and sign handlers

--- calc.js
+++ calc.js
@@ -19,3 +19,34 @@ function getResult() {
     return eval(n1() + sign() + n2());
 }
 
+$('.digit').click(function () {
+    const keyVal = $(this).text();
+    if (!sign()) {
+        n1(n1() + keyVal);
+    } else if (!equal()) {
+        n2(n2() + keyVal);
+    }
+});
+
+$('.sign').click(function () {
+    const keyVal = $(this).text();
+    if (!n1()) {
+        n1('0');
+        sign(keyVal);
+    }
+    else if (n1() && !n2()) {
+        sign(keyVal);
+    }
+    else if (n2() && !equal()) {
+        n1(getResult());
+        sign(keyVal);
+        n2('');
+    }
+    else if (equal()) {
+        let res = result();
+        clear();
+        n1(res);
+        sign(keyVal);
+    }
+});
+
