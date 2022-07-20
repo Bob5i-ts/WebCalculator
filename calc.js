82a0e1c66b297f2101f81f17602129493e722d6d
Bob5i-ts <91635735+Bob5i-ts@users.noreply.github.com>
2022-07-20 23:28:24 +0300

    Add Clear and getResult functions

--- calc.js
+++ calc.js
@@ -4,3 +4,18 @@ const sign = (...x) => $('#sign').val(...x);
 const equal = (...x) => $('#equal').val(...x);
 const result = (...x) => $('#result').val(...x);
 
+function clear() {
+    n1('');
+    n2('');
+    sign('');
+    equal('');
+    result('');
+}
+
+function getResult() {
+    if (sign() == '/' && n2() == '0') {
+        return 'Can not divide by zero';
+    }
+    return eval(n1() + sign() + n2());
+}
+
